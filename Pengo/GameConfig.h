ffb4f15184915f60049c33f38eab54061eee2fa1
Arne Olemans <Arne.olemans@telenet.be>
2024-08-27 15:02:59 +0200

    Removed Pengo Game

--- Pengo/GameConfig.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Config {
-    constexpr float ELEMENT_SIZE = 16.0f;
-    constexpr float BORDER_SIZE = 8.0f;
-
-    constexpr float MENUTOP_SIZE = 32.0f;
-    constexpr float MENUBOTTOM_SIZE = 32.0f;
-}
--- Pengo/TimerComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "TimerComponent.h"
-
-void TimerComponent::Update(float deltaTime)
-{
-	m_CurrentTime += deltaTime;
-}
